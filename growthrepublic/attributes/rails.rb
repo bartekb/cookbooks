3836ec14cf0931f1812d88801ee83e01a52944ef
Bartek Bubacz <bartosz.bubacz@polcode.pl>
2013-11-28 10:59:27 +0100

    Small

--- growthrepublic/attributes/rails.rb
+++
@@ -1,3 +0,0 @@
-include_attribute "deploy"
-
-default[:rails][:version] = "4.0.0"
--- growthrepublic/recipes/default.rb
+++
@@ -1,8 +0,0 @@
-#
-# Cookbook Name:: growthrepublic
-# Recipe:: default
-#
-# Copyright 2013, Growth Republic Ltd.
-#
-# All rights reserved - Do Not Redistribute
-#
